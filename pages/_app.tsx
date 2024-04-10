557201f7694641f75dfc3888f6359e0a92cccb1e
Kazeem Amuzat Asojuomode <77699947+amuzatk@users.noreply.github.com>
2024-04-10 17:10:20 +0100

    refactored styled component integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,39 @@
-import '../styles/global.css'
 import type { AppProps } from 'next/app'
 import { StyleSheetManager } from 'styled-components';
 import isPropValid from '@emotion/is-prop-valid';
+import { GlobalStyle } from '@/__style/global.style';
+// import '../styles/global.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <StyleSheetManager shouldForwardProp={prop => isPropValid(prop)}>
+              <GlobalStyle />
       <Component {...pageProps} />
     </StyleSheetManager>
   );
 }
 
 export default MyApp;
+
+
+
+
+
+
+
+
+
+// import '../styles/global.css'
+// import type { AppProps } from 'next/app'
+// import { StyleSheetManager } from 'styled-components';
+// import isPropValid from '@emotion/is-prop-valid';
+
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return (
+//     <StyleSheetManager shouldForwardProp={prop => isPropValid(prop)}>
+//       <Component {...pageProps} />
+//     </StyleSheetManager>
+//   );
+// }
+
+// export default MyApp;
